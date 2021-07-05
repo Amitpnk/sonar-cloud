08b7a656b29a592779d3a99077797b0ca97182cd
Amit P Naik <amit.naik8103@gmail.com>
2021-07-05 23:52:40 +0530

    refactor: removed unused namespace

--- test/Refactoring.UnitTest/CustomerTest.cs
+++ test/Refactoring.UnitTest/CustomerTest.cs
@@ -17,7 +17,7 @@ namespace Refactoring.UnitTest
             Movie avengers = new Movie("Avengers", Movie.CHILDREN);
             Rental rental = new Rental(avengers, 2);
             customer.AddRental(rental);
-            
+
             Assert.That(customer.Statement, Is.EqualTo("Rental record for Shyam\n\tAvengers\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points"));
 
         }
--- test/Refactoring.UnitTest/RentalTest.cs
+++ test/Refactoring.UnitTest/RentalTest.cs
@@ -1,10 +1,5 @@
 ﻿using NUnit.Framework;
 using Refactoring.Movies;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Refactoring.UnitTest
 {
